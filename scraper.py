1b3b2d8a0998134a9f140a0635a54d02b8fc7f7b
Hisamu Gomi <44229387+hisamugomi@users.noreply.github.com>
2026-02-20 23:34:54 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -76,7 +76,7 @@ def run_scraper():
         
         raw_data_pl = pl.from_pandas(raw_data)
 
-        raw_data_pl.filter(pl.col("genreId") == 100040)
+        raw_data_pl.filter(pl.col("genreId") == "100040")
 
         extracteddata = extract_specs(raw_data_pl, text_col="combined", price_col="itemPrice", name_col="itemName")
 
